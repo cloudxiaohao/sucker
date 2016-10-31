f01f114c3f35d775bdcdec555a872b6349a2b7ae
badguy233 <1481581056@qq.com>
2016-10-31 14:06:22 +0800

    xiugaizhongjian

--- sucker/settings.py
+++ sucker/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'tinymce',
 ]
 
-MIDDLEWARE_CLASS = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
